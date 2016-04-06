9c2cb2cbe506aeedc7d4d45e7e8423d82afe8c85
Joe Hu <i@sapika.ch>
2016-04-06 18:32:26 +0800

    Update form.js
    WIP extracting the script from HS to GitHub

--- assets/023d80bc-f2ba-4204-b5b0-9c09b32d2661/bafm/form.js
+++ assets/023d80bc-f2ba-4204-b5b0-9c09b32d2661/bafm/form.js
@@ -1,1 +1,45 @@
-// It works!
+// Async HubSpot Analytics Code
+(function(d,s,i,r) {
+if (d.getElementById(i)){return;}
+var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/234796.js';
+e.parentNode.insertBefore(n, e);
+})(document,"script","hs-analytics",300000);
+
+(function() {
+"use strict";
+
+var loadJS = function( src, cb ){
+	var ref = w.document.getElementsByTagName( "script" )[ 0 ];
+	var script = w.document.createElement( "script" );
+	script.src = src;
+	script.async = true;
+	if (cb && typeof(cb) === "function") {
+		script.onload = cb;
+	}
+	ref.parentNode.insertBefore( script, ref );
+	return script;
+};
+var loadMultipleJS = function(srcs, cb) {
+    if (!srcs.length) {
+        cb();
+        return;
+    }
+    var pendingScripts = srcs.length;
+    for (var i = 0; i < srcs.length; i++) {
+        (function() {
+            var loaded = false;
+            loadJS(srcs[i], function() {
+                if (!loaded) {
+                    loaded = true;
+                    pendingScripts--;
+                    if (pendingScripts <= 0) {
+                        cb()
+                    }
+                }
+            });
+        })();
+    }
+};
+
+})();
