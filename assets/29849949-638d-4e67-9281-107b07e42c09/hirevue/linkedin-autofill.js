f9816b50893e3dabe39838de00f8e3aaaa8266be
Joe Hu <i@sapika.ch>
2016-10-19 17:53:43 +0800

    Create linkedin-autofill.js

---
+++ assets/29849949-638d-4e67-9281-107b07e42c09/hirevue/linkedin-autofill.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function() {
+    var token;
+    var initForm = function() {
+        var forms = $(".body-container form.hs-form[id], form.hs-form[id]").first();
+        if (!forms.length) {
+            token = setTimeout(initForm, 1000);
+            return;
+        }
+        token = null;
+        $(window).off("load.liautofill");
+        form2Tag.attr("data-form", forms.attr("id"));
+        $.ajax({
+        	url: "https://www.linkedin.com/autofill/js/autofill.js",
+            dataType: "script",
+            cache: true
+        });
+    };
+    if (form2Tag.length) {
+    	initForm();
+        $(window).on("load.liautofill", function() {
+        	setTimeout(function() {
+            	clearTimeout(token);
+			}, 5000);
+        });
+    } else {
+    	console.log("LinkedIn autofill script tag is not present");
+    }
+});
