7b91eb0cd8f90a618ff725c3f2ec8eda0e49bb7a
Joe Hu <i@sapika.ch>
2017-02-21 18:17:43 +0800

    Update video.js

--- assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
+++ assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
@@ -1,4 +1,10 @@
 // Depends on: https://github.com/nfl/jquery-oembed-all
 jQuery(document).ready(function() {
-  $("a.oembed").oembed();
+  $("a.oembed").each(function() {
+    var o = $(this);
+    var href = o.attr("href");
+    if (/\bwistia\.com\b/g.test(href)) {
+      o.attr("href", href + (href.indexOf("?") ? "&" : "?") + "embedType=async_popover&videoFoam=true");
+    }
+  }).oembed();
 });
