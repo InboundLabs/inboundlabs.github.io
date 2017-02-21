8e736a972a0f8b20c23210acd7700c159d127dc2
Joe Hu <i@sapika.ch>
2017-02-21 18:32:23 +0800

    Update video.js

--- assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
+++ assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
@@ -3,8 +3,20 @@ jQuery(document).ready(function() {
   $("a.oembed").each(function() {
     var o = $(this);
     var href = o.attr("href");
+    var paramCh = (href.indexOf("?") ? "&" : "?");
+    var addDefaultParam = function(key, value) {
+      var re = new RegExp("\\?(.*&)?" + key + "=");
+      if (re.test(key)) {
+        return;
+      }
+      href += paramCh;
+      paramCh = "&";
+      href += key + "=" + value;
+    };
     if (/\bwistia\.com\b/g.test(href)) {
-      o.attr("href", href + (href.indexOf("?") ? "&" : "?") + "embedType=async_popover&videoFoam=true");
+      addDefaultParam("embedType", "async_popover");
+      addDefaultParam("videoFoam", "true");
     }
+    o.attr("href", href);
   }).oembed();
 });
