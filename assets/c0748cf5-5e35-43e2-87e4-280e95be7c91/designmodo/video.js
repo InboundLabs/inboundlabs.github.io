bd817ba920f52b43c8c3cf8b5899fe26bfc35496
Joe Hu <i@sapika.ch>
2018-05-14 21:23:05 +0800

    Use background mode for Vimeo

--- assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
+++ assets/c0748cf5-5e35-43e2-87e4-280e95be7c91/designmodo/video.js
@@ -42,12 +42,10 @@ jQuery(document).ready(function() {
       if (bgMode) {
         onBeforeEmbed = function(container, data) {
           if (container && container.code) {
-            container.code = container.code.replace(/\bvimeo.com\/[^'"]+/g, "$&?autopause=0&autoplay=1&loop=1&title=0");
+            container.code = container.code.replace(/\bvimeo.com\/[^'"]+/g, "$&?background=1&title=0");
           }
         };
-        addDefaultParam("autopause", "false");
-        addDefaultParam("autoplay", "true");
-        addDefaultParam("loop", "true");
+        addDefaultParam("background", "1");
       }
     }
     o.attr("href", href);
