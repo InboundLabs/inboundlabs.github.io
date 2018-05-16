89eff571963698afc294b769af5d08a6a4ea830e
shamaleyte <serhatsama@gmail.com>
2018-05-16 14:21:08 +0300

    Update authenticate.js

--- assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
+++ assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
@@ -1,24 +1,3 @@
-<!-- https://github.com/js-cookie/js-cookie  -->
-<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
-<!-- jQuery 1.7.2+ or Zepto.js 1.0+ -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<!-- Magnific Popup core JS file -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-
-
-
-<!-- link that opens popup -->
-<a class="popup-modal" href="#test-modal">Open modal</a>
-
-<div id="test-modal" class="white-popup-block mfp-hide">
-	<form id="auth-form">
-	<h1>Authenticate</h1>
-	<fieldset style="border:0;"> <ul> <li> <label for="name">Password</label>
-	<input id="pwd-field" type="password" name="psw">
-	<input type="submit" value="Submit"> </li> </ul> </fieldset> </form>
-</form>
-</div>
-
 
 <script>
     $(document).ready(function() {
@@ -47,19 +26,3 @@ $( "#auth-form" ).submit(function( event ) {
 });
 });
 </script>
-<style>
-    #auth-form{
-    background: #FFF;
-    padding: 20px 30px;
-    text-align: left;
-    max-width: 650px;
-    margin: 40px auto;
-    position: relative;
-    }
-    #auth-form ul{
-            list-style: none;
-    }
-    #auth-form ul label, #auth-form ul input{
-        margin-right:10px;
-    }
-</style>
