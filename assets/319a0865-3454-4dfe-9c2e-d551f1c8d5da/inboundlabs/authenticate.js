7e479ac23401f202a209c115436574a50e0344c4
shamaleyte <serhatsama@gmail.com>
2018-05-16 14:26:42 +0300

    Update authenticate.js

--- assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
+++ assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
@@ -1,1 +1,25 @@
 
+    $(document).ready(function() {
+    var cookie_val = Cookies.get('authenticated');
+    if(cookie_val == "true")
+        console.log("exit script");
+    else{
+        console.log("proceed with script");
+
+        $.magnificPopup.open({
+          items: {
+            src: '#test-modal'
+          },
+        	type: 'inline',
+        		preloader: false,
+        		focus: '#username',
+        		modal: true
+        });
+}
+
+$( "#auth-form" ).submit(function( event ) {
+  Cookies.set('authenticated', 'true');
+  event.preventDefault();
+  $.magnificPopup.close();
+});
+});
