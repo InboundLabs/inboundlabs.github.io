47eda643c85bd7a020892a97998ab2afbce1fe84
shamaleyte <serhatsama@gmail.com>
2018-05-16 16:54:31 +0300

    Update authenticate.js

--- assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
+++ assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
@@ -1,10 +1,9 @@
 	var url_magnific = "https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js";
-    var re = new RegExp('[; ]'+'il_internal_auth'+'=([^\\s;]*)');
-    var sMatch = (' '+document.cookie).match(re);
-    if(sMatch)
-		    console.log("exit script");
-	else{
-	$.getScript(url_magnific, function() {
+	var re = new RegExp('[; ]' + 'il_internal_auth' + '=([^\\s;]*)');
+	var sMatch = (' ' + document.cookie).match(re);
+	if (sMatch) console.log("exit script");
+	else {
+		$.getScript(url_magnific, function() {
 			console.log("proceed with script");
 			$('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">').appendTo("head");
 			var modal = '<div id="test-modal" class="white-popup-block mfp-hide">' + '<form id="auth-form">' + '<h1>Authenticate</h1>' + '<fieldset style="border:0;"> <ul> <li> <label for="name">Password</label>' + '<input id="pwd-field" type="password" name="psw">' + '<input type="submit" value="Submit"> </li> </ul> </fieldset> </form></div>';
@@ -18,16 +17,15 @@
 				focus: '#username',
 				modal: true
 			});
-		
-		$("#auth-form").submit(function(event) {
-			document.cookie = "il_internal_auth=true";
-			event.preventDefault();
-			$.magnificPopup.close();
+			$("#auth-form").submit(function(event) {
+				document.cookie = "il_internal_auth=true";
+				event.preventDefault();
+				$.magnificPopup.close();
+			});
+			var css = document.createElement("style");
+			css.type = "text/css";
+			css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px;text-align: left;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "#auth-form h1{    margin-top: 0;    }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px;    }";
+			document.body.appendChild(css);
+			var link = '<a class="popup-modal" href="#test-modal">Open modal</a>';
 		});
-		var css = document.createElement("style");
-		css.type = "text/css";
-		css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px;text-align: left;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "#auth-form h1{    margin-top: 0;    }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px;    }";
-		document.body.appendChild(css);
-		var link = '<a class="popup-modal" href="#test-modal">Open modal</a>';
-	});
-		}
+	}
