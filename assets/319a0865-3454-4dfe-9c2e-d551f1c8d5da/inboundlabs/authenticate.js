39305ed2495817f6121420bad82fe213a6dadbe2
Joe Hu <i@sapika.ch>
2018-05-22 17:51:56 +0800

    Update authenticate.js

--- assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
+++ assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
@@ -7,7 +7,7 @@
 			$('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">').appendTo("head");
 			var modal = '<div id="test-modal" class="white-popup-block mfp-hide">' + '<form id="auth-form">' + '<h1>Authenticate</h1>' + '<fieldset style="border:0;"> <ul> <li> <label for="name">Password</label>' + '<input id="pwd-field" type="password" name="psw">' + '<input type="submit" value="SUBMIT"> </li> </ul> </fieldset> <label style="color: red;">Login failed: Please make sure your password is correct.</label></form></div>';
 			$("body").append(modal);
-			$("#auth-form label").hide();
+			$("#auth-form label").css("visibility", "hidden");
 			$.magnificPopup.open({
 				items: {
 					src: '#test-modal'
@@ -20,7 +20,7 @@
 			$("#auth-form").submit(function(event) {
   				event.preventDefault();
 				if($("#pwd-field").val() != "inboundLabs"){					
-				  $("#auth-form label").show();
+				  $("#auth-form label").css("visibility", "");
 			          $("#auth-form").addClass("login-failed");
 				}
 				else{
@@ -33,8 +33,7 @@
 			});
 			var css = document.createElement("style");
 			css.type = "text/css";
-			css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px; padding-bottom: 20px; text-align: center;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "form#auth-form.login-failed {     background: #e9e9e9;  }" + "#auth-form h1{    margin-top: 0;  font-size:24px;  }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px; display:block; margin: 10px auto;   }" + ".mfp-inline-holder .mfp-content, .mfp-ajax-holder .mfp-content {width:auto;}" + "#auth-form ul input[type='submit']{display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; border: 1px solid transparent; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out; color: #007bff; background-color: transparent; background-image: none; border-color: #007bff; cursor: pointer; width:100%;}" + "#auth-form ul input[type='submit']:hover{color: #fff; background-color: #007bff; border-color: #007bff;}" + "#auth-form ul input#pwd-field{    padding: 1em;     border: 1px solid rgba(0,0,0,.12);}";
+			css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px; padding-bottom: 20px; text-align: center;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "#auth-form h1{    margin-top: 0;  font-size:24px;  }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px; display:block; margin: 10px auto;   }" + ".mfp-inline-holder .mfp-content, .mfp-ajax-holder .mfp-content {width:auto;}" + "#auth-form ul input[type='submit']{display: inline-block; font-weight: 400; text-align: center; white-space: nowrap; vertical-align: middle; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; border: 1px solid transparent; padding: .375rem .75rem; font-size: 1rem; line-height: 1.5; border-radius: .25rem; transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out; color: #007bff; background-color: transparent; background-image: none; border-color: #007bff; cursor: pointer; width:100%;}" + "#auth-form ul input[type='submit']:hover{color: #fff; background-color: #007bff; border-color: #007bff;}" + "#auth-form ul input#pwd-field{    padding: 1em;     border: 1px solid rgba(0,0,0,.12);}";
 			document.body.appendChild(css);
-			var link = '<a class="popup-modal" href="#test-modal">Open modal</a>';
 		});
 	}
