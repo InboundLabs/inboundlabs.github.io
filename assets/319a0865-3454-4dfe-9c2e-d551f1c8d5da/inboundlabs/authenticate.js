5a67299afe78982ccfec6df8f6795370fa969f67
shamaleyte <serhatsama@gmail.com>
2018-05-22 10:18:10 +0300

    Update authenticate.js

--- assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
+++ assets/319a0865-3454-4dfe-9c2e-d551f1c8d5da/inboundlabs/authenticate.js
@@ -5,8 +5,9 @@
 		$.getScript(url_magnific, function() {
 			console.log("proceed with script");
 			$('<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">').appendTo("head");
-			var modal = '<div id="test-modal" class="white-popup-block mfp-hide">' + '<form id="auth-form">' + '<h1>Authenticate</h1>' + '<fieldset style="border:0;"> <ul> <li> <label for="name">Password</label>' + '<input id="pwd-field" type="password" name="psw">' + '<input type="submit" value="Submit"> </li> </ul> </fieldset> </form></div>';
+			var modal = '<div id="test-modal" class="white-popup-block mfp-hide">' + '<form id="auth-form">' + '<h1>Authenticate</h1>' + '<fieldset style="border:0;"> <ul> <li> <label for="name">Password</label>' + '<input id="pwd-field" type="password" name="psw">' + '<input type="submit" value="Submit"> </li> </ul> </fieldset> <label style="color: red;">Login failed: Please make sure your password is correct.</label></form></div>';
 			$("body").append(modal);
+			$("#auth-form label").hide();
 			$.magnificPopup.open({
 				items: {
 					src: '#test-modal'
@@ -17,15 +18,20 @@
 				modal: true
 			});
 			$("#auth-form").submit(function(event) {
+  				event.preventDefault();
+				if($("#pwd-field").val() != "inboundLabs"){					
+				  $("#auth-form label").show();
+				}
+				else{
 				$.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js", function() {
 					Cookies.set('il_internal_auth', 'true');				
-				});
-				event.preventDefault();
+				});				
 				$.magnificPopup.close();
+				}
 			});
 			var css = document.createElement("style");
 			css.type = "text/css";
-			css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px;text-align: center;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "#auth-form h1{    margin-top: 0;  font-size:24px;  }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px; display:block; margin: 10px auto;   }" + ".mfp-inline-holder .mfp-content, .mfp-ajax-holder .mfp-content {width:auto;}";
+			css.innerHTML = "#auth-form{background: #FFF;padding: 50px 30px; padding-bottom: 20px; text-align: center;max-width: 650px;margin: 40px auto;position: relative;font-family: Helvetica,Arial,sans-serif;}" + "#auth-form h1{    margin-top: 0;  font-size:24px;  }" + "#auth-form fieldset {        padding:0;    }" + "#auth-form ul{    list-style: none;    margin: 0;    padding: 0;    }" + "#auth-form ul label, #auth-form ul input{        margin-right:10px;    }" + "#auth-form ul input{        padding-left: 5px; display:block; margin: 10px auto;   }" + ".mfp-inline-holder .mfp-content, .mfp-ajax-holder .mfp-content {width:auto;}";
 			document.body.appendChild(css);
 			var link = '<a class="popup-modal" href="#test-modal">Open modal</a>';
 		});
